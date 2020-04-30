7d36b92e109ad4b4132e1d14739034d07c681e4c
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-30 11:03:49 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_reader :backers, :title
+  
+def initialize(title)
+  @title = title
+  @backers = []
+end
+
+def add_backer(backer)
+  project = project.new
+  project.title.new
+  @backers << self
+end
+end
